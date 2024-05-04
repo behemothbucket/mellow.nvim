e978c9d1371139c5da148cce7fe19618d99ea325
behemothbucket <mahagon.here@gmail.com>
2024-05-04 20:28:28 +0300

    init

--- lua/mellow/init.lua
+++ lua/mellow/init.lua
@@ -282,7 +282,7 @@ local set_groups = function()
     ["HopPreview"] = { fg = c.bright_blue },
 
     -- Flash
-    ["FlashLabel"] = { fg = c.cyan },
+    ["FlashLabel"] = { fg = c.blue },
 
     -- Cmp
     ["CmpItemAbbrDeprecated"] = { fg = c.gray06, strikethrough = true },
